6080cb216d241aca6c5214a45b1cd27fed8e2401
Edwin Siebel <edwin@yard.nl>
2018-10-03 11:24:59 +0200

    Update: docs, languages

--- pdc-leges.php
+++ pdc-leges.php
@@ -18,19 +18,19 @@ use OWC\PDC\Leges\Foundation\Plugin;
 /**
  * If this file is called directly, abort.
  */
-if ( ! defined('WPINC')) {
+if (!defined('WPINC')) {
     die;
 }
 
 // Don't boot if base plugin is not active.
-if ( ! is_plugin_active('pdc-base/pdc-base.php')) {
+if (!is_plugin_active('pdc-base/pdc-base.php')) {
     return;
 }
 
 /**
  * manual loaded file: the autoloader.
  */
-require_once __DIR__.'/autoloader.php';
+require_once __DIR__ . '/autoloader.php';
 $autoloader = new Autoloader();
 
 /**
@@ -40,6 +40,6 @@ $autoloader = new Autoloader();
  * plugin overrides. The plugins_loaded action hook fires early, and precedes the setup_theme, after_setup_theme, init
  * and wp_loaded action hooks.
  */
-add_action('plugins_loaded', function() {
+add_action('plugins_loaded', function () {
     $plugin = (new Plugin(__DIR__))->boot();
-}, 9);
+}, 10);
